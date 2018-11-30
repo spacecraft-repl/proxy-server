c1747ee4209c2348b6deb96b63054baca75477ef
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2018-11-30 01:54:40 +0000

    add proxy to 2 containers

--- proxy-server.js
+++ proxy-server.js
@@ -3,13 +3,15 @@ const proxy = require('redbird')({ port: 80, xfwd: false })
 // const app = express()
 let sessions = {}
 
-sessions['first'] = "proxy-fix";
+sessions['first'] = "172.17.0.2:3000"
+sessions['second'] = "172.17.0.3:3000"
 
 // proxy.register('spacecraft-repl.com/first', sessions['first'])
 // proxy.register('spacecraft-repl.com/', sessions['first'])
 
-const docker = require('redbird').docker
-docker(proxy).register('spacecraft-repl.com', sessions['first'])
-docker(proxy).register('spacecraft-repl.com/first', sessions['first'])
+// const docker = require('redbird').docker
+// proxy.register('spacecraft-repl.com', sessions['first'])
+proxy.register('spacecraft-repl.com/first', sessions['first'])
+proxy.register('spacecraft-repl.com/second', sessions['second'])
  
 
