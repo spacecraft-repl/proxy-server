d524e111c05c8d93e0649a42d3a812f5f297c6ae
Julius Zerwick <jrzerwick@gmail.com>
2018-12-01 14:46:38 -0600

    Created Promise

--- server.js
+++ server.js
@@ -9,38 +9,37 @@ let sessions = {}
 const proxy = require('redbird')({
   port: 80,
   resolvers: [
-    function(host, url, request) {
+    async function(host, url, request) {
       
       if(host === 'spacecraft-repl.com' && url === '/') {
         let sessionId = Math.floor(Math.random() * 1000)
 
-        docker.createContainer({
-          Image: 'proxy-fix',
-          Tty: false,
-          ExposedPorts: { "3000/tcp": {} },
-          HostConfig: {
-            Runtime: 'runsc',
-            Memory: 100000000,
-            CpuPeriod: 100000,
-            CpuQuota:   20000,
-          }
-        }, function (err, container) {
-          container.start(function (err, data) {
-            container.inspect(container.id).then(data => {
-              const IPAddress = data.NetworkSettings.IPAddress
-              sessions[sessionId] = IPAddress
-              // proxy.register('spacecraft-repl.com',`${sessionId}.${DOMAIN}`)
-              // proxy.register(`${sessionId}.${DOMAIN}`, `${IPAddress}:${PORT}`)
+        await new Promise(function(resolve, reject) {
+          docker.createContainer({
+            Image: 'proxy-fix',
+            Tty: false,
+            ExposedPorts: { "3000/tcp": {} },
+            HostConfig: {
+              Runtime: 'runsc',
+              Memory: 100000000,
+              CpuPeriod: 100000,
+              CpuQuota:   20000,
+            }
+          }, function (err, container) {
+            container.start(function (err, data) {
+              container.inspect(container.id).then(data => {
+                const IPAddress = data.NetworkSettings.IPAddress
+                sessions[sessionId] = IPAddress
+                proxy.register(`${sessionId}.${DOMAIN}`, `${IPAddress}:${PORT}`)
+                resolve()
+              })
             })
-          })
+          })  
         })
-        // proxy.register(`${sessionId}.${DOMAIN}`, `${IPAddress}:${PORT}`)
+        return `${sessions[sessionId]}:${PORT}`
       }
     }
   ]
 })
-proxy.register(`${sessionId}.${DOMAIN}`, `${IPAddress}:${PORT}`)
-// proxy.register('spacecraft-repl.com', 'spacecraft-repl.com') 
 
-console.log(proxy)
-
+// proxy.register('spacecraft-repl.com',`${sessionId}.${DOMAIN}`)
