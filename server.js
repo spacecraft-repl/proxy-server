befcf271927ef71fd55efe11cc35621230493407
Julius Zerwick <jrzerwick@gmail.com>
2018-12-01 14:00:48 -0600

    refactored code base

--- server.js
+++ server.js
@@ -2,24 +2,18 @@
 const Docker = require('dockerode')
 let docker = new Docker({socketPath: '/var/run/docker.sock'})
 
-const execFileSync = require('child_process').execFileSync
-
 const DOMAIN = 'spacecraft-repl.com'
 const PORT   = 3000
 let sessions = {}
-//   [`a.${DOMAIN}`]: `172.17.0.2:${PORT}`,
-//   [`b.${DOMAIN}`]: `172.17.0.3:${PORT}`
-// }
 
 const proxy = require('redbird')({
   port: 80,
   resolvers: [
     function(host, url, request) {
-      // console.log(`host is ${host} and url is ${url}`);
-      // if(host === 'localhost') {
+      
       if(host === 'spacecraft-repl.com' && url === '/') {
         let sessionId = Math.floor(Math.random() * 1000)
-        let containerNetwork = execFileSync('./dockerscript.sh')
+
         docker.createContainer({
           Image: 'proxy-fix',
           Tty: false,
@@ -41,31 +35,10 @@ const proxy = require('redbird')({
             })
           })
         })
-        // console.log(containerNetwork + '')
-        
-        // console.log(`sessionId is ${sessionId}`)
       }
     }
   ]
 })
 
-// let sessions = [
-//   {
-//     src:    `a.${DOMAIN}`,
-//     target: `172.17.0.2:${PORT}`,
-//   },
-//   {
-//     src:    `b.${DOMAIN}`,
-//     target: `172.17.0.3:${PORT}`,
-//   },
-// ]
 
-
-// const registerSessionRoute = ({src, target}) => {
-//   proxy.register(src, target)
-// }
-
-// Object.entries(sessions).forEach( ([src, target]) => {
-//   proxy.register(src, target)
-// })
 
