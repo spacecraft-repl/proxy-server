bb22f1e542bfb178be9544b5d05f0c8a02cbeaee
Julius Zerwick <jrzerwick@gmail.com>
2018-11-30 18:59:14 -0600

    Refactored server.js

--- server.js
+++ server.js
@@ -7,19 +7,26 @@ const proxy = require('redbird')({
 const DOMAIN = 'spacecraft-repl.com'
 const PORT   = 3000
 
-let sessions = [
-  {
-    src:    `a.${DOMAIN}`,
-    target: `172.17.0.2:${PORT}`,
-  },
-  {
-    src:    `b.${DOMAIN}`,
-    target: `172.17.0.3:${PORT}`,
-  },
-]
+// let sessions = [
+//   {
+//     src:    `a.${DOMAIN}`,
+//     target: `172.17.0.2:${PORT}`,
+//   },
+//   {
+//     src:    `b.${DOMAIN}`,
+//     target: `172.17.0.3:${PORT}`,
+//   },
+// ]
+
+let sessions = {
+  [`a.${DOMAIN}`]: `172.17.0.2:${PORT}`,
+  [`b.${DOMAIN}`]: `172.17.0.3:${PORT}`
+}
 
 const registerSessionRoute = ({src, target}) => {
   proxy.register(src, target)
 }
 
-sessions.forEach(registerSessionRoute)
+Object.entries(sessions).forEach( ([src, target]) => {
+  proxy.register(src, target)
+})
